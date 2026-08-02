35b9675be9916fbc3296e9b54bc07db3ed22d2ec
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-02 19:41:34 +0800

    Add rgblight info

--- keymap.c
+++ keymap.c
@@ -31,6 +31,14 @@ bool render_status(void) {
     oled_write_P(PSTR("wpm:"), false);
     oled_write_ln(get_u8_str(get_current_wpm(), ' '), false);
 
+    oled_write(get_u8_str(rgblight_get_mode(), ' '), false);
+    oled_write(" ", false);
+    oled_write(get_u8_str(rgblight_get_hue(), ' '), false);
+    oled_write(" ", false);
+    oled_write(get_u8_str(rgblight_get_sat(), ' '), false);
+    oled_write(" ", false);
+    oled_write_ln(get_u8_str(rgblight_get_val(), ' '), false);
+
     led_t led_state = host_keyboard_led_state();
     oled_write_P(led_state.num_lock ? PSTR("num ") : PSTR("    "), false);
     oled_write_P(led_state.caps_lock ? PSTR("cap ") : PSTR("    "), false);
