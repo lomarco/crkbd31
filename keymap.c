7eb03b6352bed94c1d87337029cfa62a4a2e7d5b
lomarco <119614090+lomarco@users.noreply.github.com>
2026-08-02 17:22:05 +0800

    Add new oled functions

--- keymap.c
+++ keymap.c
@@ -7,6 +7,56 @@ enum layers {
     _AJ
 };
 
+#ifdef OLED_ENABLE
+bool render_status(void) {
+    oled_write_P(PSTR("layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _DF:
+            oled_write_P(PSTR("default\n"), false);
+            break;
+        case _LW:
+            oled_write_P(PSTR("lower\n"), false);
+            break;
+        case _RS:
+            oled_write_P(PSTR("raise\n"), false);
+            break;
+        case _AJ:
+            oled_write_P(PSTR("adjust\n"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("undefined"), false);
+    }
+
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("num ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("cap ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("scr ") : PSTR("    "), false);
+
+    return false;
+}
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        render_status();
+    } else {
+        render_logo();
+        oled_scroll_left();
+    }
+    return false;
+}
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DF] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
